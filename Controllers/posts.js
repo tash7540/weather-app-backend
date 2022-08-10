c4c50d93686db496375126d61518da2508f2e85d
tash7540 <59746687+tash7540@users.noreply.github.com>
2022-08-09 22:31:03 -0600

    finish preprocessing forecast data

--- Controllers/posts.js
+++ Controllers/posts.js
@@ -21,7 +21,6 @@ export const citySearch = async (req, res) => {
         } else {
             let weather = JSON.parse(body);
             // you shall output it in the console just to make sure that the data being displayed is what you want
-            console.log(weather);
             if (weather.main == undefined) {
                 res.json({ error: err })
 
@@ -38,7 +37,7 @@ export const citySearch = async (req, res) => {
 
                 let currdate = weatherTimezone.slice(4,15);
                 var dayName = days[testday.getDay()];
-                console.log(currdate);
+                console.log("TT"+dayName);
                 let weatherTemp = `${weather.main.temp}`,
                   weatherPressure = `${weather.main.pressure}`,
                   /* you will fetch the weather icon and its size using the icon data*/
@@ -54,10 +53,10 @@ export const citySearch = async (req, res) => {
                 weatherDescription1 = weatherDescription1.map((word)=> word[0].toUpperCase()+word.slice(1,word.length));
                 console.log(weatherDescription1);
                 weatherDescription = weatherDescription1[0]+' '+weatherDescription1[1];
-                let direction = utils.getCardinalDirection(weather.wind.deg);
+                let direction = getCardinalDirection(weather.wind.deg);
                 let windSpeed = `${weather.wind.speed} m/s ${direction}`;
 
-                weatherFahrenheit = utils.roundToTwo(weatherFahrenheit);
+                weatherFahrenheit = roundToTwo(weatherFahrenheit);
                 // you shall now render the data to your page (index.ejs) before displaying it out
                 res.json({
                   weather: weather,
@@ -86,6 +85,7 @@ export const citySearchForecast = async (req, res) => {
 
     // Get city name passed in the form
     let city = req.body.city;
+    const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
     // Use that city name to fetch data
     // Use the API_KEY in the '.env' file
@@ -100,54 +100,41 @@ export const citySearchForecast = async (req, res) => {
 
         } else {
             var weather = JSON.parse(body);
+            console.log(weather);
 
-            // you shall output it in the console just to make sure that the data being displayed is what you want
-            let weather_grouped = (weather['list']).map(utils.groupday);
-
-            var data_min = {};
-            var data_max = {};
-
+            let weather_grouped = (weather['list']).map(groupday);
+            var data_temps = {};
+            var data_icons = {};
+            var data_days = {};
             for(var i in weather_grouped){
-              data_min[Object.keys(weather_grouped[i])[0]] = [];
-              data_max[Object.keys(weather_grouped[i])[0]] = [];
+              data_temps[Object.keys(weather_grouped[i])[0]] = [];
+              data_icons[Object.keys(weather_grouped[i])[0]] = "";
+              data_days[Object.keys(weather_grouped[i])[0]] = "";
 
             }
             for(var i in weather_grouped){
               var a = Object.keys(weather_grouped[i])[0];
-              data_min[Object.keys(weather_grouped[i])[0]].push(weather_grouped[i][a][0]['main']['temp_min']);
-              data_max[Object.keys(weather_grouped[i])[0]].push(weather_grouped[i][a][0]['main']['temp_max']);
+              data_temps[Object.keys(weather_grouped[i])[0]].push(weather_grouped[i][a][0]['main']['temp_max']);
+              data_icons[Object.keys(weather_grouped[i])[0]] = weather_grouped[i][a][0]['weather'][0]['icon'];
+              var testday =new Date(weather_grouped[i][a][0]['dt'] * 1000);
+              data_days[Object.keys(weather_grouped[i])[0]] =days[testday.getDay()];
 
-            }
-            const temps_min = [];
-            const temps_max = [];
-
-            for(var i in data_min){
-              var sum_min = 0;
-              var sum_max = 0;
-
-              for(var j in data_min[i]){
-                sum_min = sum_min+data_min[i][j];
-              }
-              for(var j in data_max[i]){
-                sum_max = sum_max+data_max[i][j];
-              }
-              temps_min.push(sum_min/data_min[i].length);
-              temps_max.push(sum_max/data_max[i].length);
 
             }
 
-            res.json({
-              day1_min:utils.roundToTwo(temps_min[0]),
-              day2_min:utils.roundToTwo(temps_min[1]),
-              day3_min:utils.roundToTwo(temps_min[2]),
-              day4_min:utils.roundToTwo(temps_min[3]),
-              day5_min:utils.roundToTwo(temps_min[4]),
-              day1_max:utils.roundToTwo(temps_max[0]),
-              day2_max:utils.roundToTwo(temps_max[1]),
-              day3_max:utils.roundToTwo(temps_max[2]),
-              day4_max:utils.roundToTwo(temps_max[3]),
-              day5_max:utils.roundToTwo(temps_max[4])
-            });
+            const data = [];
+            for(const i in data_temps){
+              var summ = 0;
+              for(var j in data_temps[i]){
+                summ = summ+data_temps[i][j];
+              }
+              var temp = summ /data_temps[i].length;
+              var obj = {"temp":roundToTwo(temp),
+              "day":data_days[i],
+              "icon":data_icons[i]};
+              data.push(obj);
+            }
+            res.json(data);
 
             }
         })
